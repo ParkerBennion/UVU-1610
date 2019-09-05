8090859d8cb6bde661da64fe524cb43db6fce8ba
Parker <parkerbennion@gmail.com>
2019-09-05 14:46:22 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ proj_test/Assets/flats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class flats : MonoBehaviour
+{
+	public float floatValue = 5.5f;
+	public int intValue = 20;
+	public int firepower;
+	public string stringValue = "bob";
+	public UnityEvent Event;
+	public UnityEvent stoppingevent;
+	
+
+	private void OnTriggerEnter(Collider other)
+	{
+		print(stringValue);
+		Event.Invoke();
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		stoppingevent.Invoke();
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
