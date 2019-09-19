1c58a1091b25023bd34d1da7200baa35356dfe24
Parker <parkerbennion@gmail.com>
2019-09-19 16:08:43 -0600

    cleaned prev code. and made jump code

---
+++ proj_test/Assets/Gravity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+	private int jumpCount;
+	public int jumpCountMax = 2;
+	public Vector3 position;
+	public float moveSpeed = 10f, grav = 9.3f, jumpSpeed = 30;
+
+	private CharacterController _controller;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		position.x = moveSpeed* Input.GetAxis("Horizontal");
+		position.y -= grav; // put negative before "=" if your adding jumping.
+		
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+		{
+			position.y = jumpSpeed;
+			jumpCount++;
+		}
+
+		if (_controller.isGrounded)
+		{
+			position.y = 0;
+			jumpCount = 0;
+		}
+	}
+}
--- proj_test/Assets/Rotating.cs
+++ proj_test/Assets/Rotating.cs
@@ -1,35 +1,22 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+//this code demands a character controller and will make one when this script is applied
 public class Rotating : MonoBehaviour
 {
-	//public GeometryUtility rotar; not what i needed
-	//public PivotRotation rotam; not what i needed
-	public CharacterController controller;
-	public float rotate = 10f;
-	public Vector3 rotation;
-
-	public Vector3 position;
+	private CharacterController controller;
 	public float speed = 100f;
-	public float gravity = 3.4f;
+	public Vector3 position;
 
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
 	void Update () {
-		//if (Input.GetButtonDown("Mouse X"))
-		//{
-		//	transform.rotation.x = position;
-		//}
-		rotation.x = rotate*Input.GetAxis("Mouse X");
-		//rotation.y = rotate*Input.GetAxis("Mouse Y");
-		
-		//moved with mouse 
-		//now going to attempt to apply gravity
-		
-		if (!controller.isGrounded)
-		{
-			position.y -= gravity;
-		}
-		//position shows to be falling but game object appears not to fall???
-		controller.Move(rotation*Time.deltaTime);
+		position.x = speed*Input.GetAxis("Mouse X");
+		position.y = speed*Input.GetAxis("Mouse Y");
+
+		controller.Move(position*Time.deltaTime);
 		
 	}
 }
