8fbf0cb4b2f841b5a0978cb9e5eadcd73558b276
Parker <parkerbennion@gmail.com>
2019-09-18 22:23:38 -0600

    make character move with mouse (rotation)

---
+++ proj_test/Assets/Controls.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Controls : MonoBehaviour
+{
+	public Vector3 position;
+	public CharacterController controller;
+	public float speed = 100f;
+	public float gravity = 3.4f;
+	public float jumpSpeed = 10f;
+	public float rotate = 90;
+	
+	void Update () {
+		position.x = speed*Input.GetAxis("Horizontal");
+		if (!controller.isGrounded)
+		{
+			position.y -= gravity;
+		}
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+
+		controller.Move(position*Time.deltaTime);
+	}
+}
---
+++ proj_test/Assets/Rotating.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+	//public GeometryUtility rotar; not what i needed
+	//public PivotRotation rotam; not what i needed
+	public CharacterController controller;
+	public float rotate = 10f;
+	public Vector3 rotation;
+
+	public Vector3 position;
+	public float speed = 100f;
+	public float gravity = 3.4f;
+
+	void Update () {
+		//if (Input.GetButtonDown("Mouse X"))
+		//{
+		//	transform.rotation.x = position;
+		//}
+		rotation.x = rotate*Input.GetAxis("Mouse X");
+		//rotation.y = rotate*Input.GetAxis("Mouse Y");
+		
+		//moved with mouse 
+		//now going to attempt to apply gravity
+		
+		if (!controller.isGrounded)
+		{
+			position.y -= gravity;
+		}
+		//position shows to be falling but game object appears not to fall???
+		controller.Move(rotation*Time.deltaTime);
+		
+	}
+}
