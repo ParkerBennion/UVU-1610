12f797d3e5751cc1381f2d0a938f4b9f86142938
Parker <parkerbennion@gmail.com>
2019-09-23 23:50:39 -0600

    made rock gravity prog.

---
+++ proj_test/Assets/Rock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Rock : MonoBehaviour
+{
+	public float gravity = 9.3f;
+	public Vector3 place;
+	private CharacterController _controller;
+	private void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+	}
+	void Update () {
+		place.y = gravity; // put negative before "=" if your adding jumping.
+		
+		if (_controller.isGrounded)
+		{
+			gravity = 0;
+		}
+		_controller.Move(place*Time.deltaTime);
+		
+	}
+}
