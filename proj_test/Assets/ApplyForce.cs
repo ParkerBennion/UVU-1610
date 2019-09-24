90260f8c403b40c1afcfc0b5f8f516af6e563e4a
Parker <parkerbennion@gmail.com>
2019-09-24 15:52:02 -0600

    made if statements

---
+++ proj_test/Assets/ApplyForce.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ApplyForce : MonoBehaviour
+{
+	public Vector3 forces;
+	private Rigidbody rb;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		rb.AddForce(forces);
+	}
+}
---
+++ proj_test/Assets/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+	public int a = 4;
+	public int b = 5;
+	public int c = 10;
+	public string password = "0U812";
+	public bool lightOn = true;
+	void Start () {
+		if (a+b == c)
+		{
+			print(true);
+		}
+		else
+		{
+			print(false);
+		}
+		if (password == "OU812") //(password != "OU812") /anything but OU812 is true.
+		{
+			print(true);
+		}
+
+		if (!lightOn) //"!" means if this is not true print true.
+		{
+			print(true);
+		}
+	}
+	// "||" this means or.
+	void Update () {
+		
+	}
+}
