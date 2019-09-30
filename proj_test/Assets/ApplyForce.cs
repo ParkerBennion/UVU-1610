6c41f831c2ef9d6c12a0c0f67bcbda416330676d
Parker <parkerbennion@gmail.com>
2019-09-29 23:49:46 -0600

    combined functions

--- proj_test/Assets/ApplyForce.cs
+++ proj_test/Assets/ApplyForce.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]  
 [RequireComponent(typeof(Collider))]
 public class ApplyForce : MonoBehaviour
 {
---
+++ proj_test/Assets/Combine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combine : MonoBehaviour {
+	public ParticleSystem sparks;
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Collider))]
+	// Use this for initialization
+	public class ApplyForce : MonoBehaviour
+	{
+		public Vector3 forces;
+		private Rigidbody rb;
+		void Start ()
+		{
+			rb = GetComponent<Rigidbody>();
+		}
+
+		private void OnCollisionEnter(Collision other)
+		{
+			rb.AddForce(forces);
+		}
+	public void OnMouseDown()
+	{
+		sparks.Emit(10);
+	}
+}
