2eb592a78d7d0c910c6053a051d02774b88615e8
Parker <parkerbennion@gmail.com>
2019-10-01 15:52:06 -0600

    a bunch of shitty script sorry i know im not supposed to.

---
+++ proj_test/Assets/IntDataObj.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu]
+public class IntDataObj : ScriptableObject
+{
+	public int value = 1;
+
+	public void UpdateValue(int number)
+	{
+		value += number;
+	}
+}
---
+++ proj_test/Assets/UiTextCont.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UiTextCont : MonoBehaviour
+{
+	private Text textLabel;
+	public UnityEvent startDisplay;
+	public void UpdateText(IntDataObj data)
+	{
+		textLabel.text = data.value.ToString();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		textLabel = GetComponent<Text>();
+		startDisplay.Invoke();
+	}
+}
