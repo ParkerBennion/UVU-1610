7e6707f2886afea273448079a924429d5a794e70
Parker <parkerbennion@gmail.com>
2019-10-03 15:55:46 -0600

    helthbar UI

---
+++ proj_test/Assets/UiImmageCont.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UiImmageCont : MonoBehaviour
+{
+	private Image img;
+	void Awake ()
+	{
+		img = GetComponent<Image>();
+	}
+
+	public void UpdateImage(FloatData data)
+	{
+		img.fillAmount = data.value;
+	}
+
+	void Update () {
+		
+	}
+}
--- proj_test/Assets/UiTextCont.cs
+++ proj_test/Assets/UiTextCont.cs
@@ -15,7 +15,7 @@ public class UiTextCont : MonoBehaviour
 	}
 
 	// Use this for initialization
-	void Start ()
+	void Awake ()
 	{
 		
 		textLabel = GetComponent<Text>();
