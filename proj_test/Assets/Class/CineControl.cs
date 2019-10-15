916cf79b51b1d5bd20dd32b4928e8db53ce23dbe
Parker <parkerbennion@gmail.com>
2019-10-15 15:43:15 -0600

    made a follow camera

---
+++ proj_test/Assets/Class/CineControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Cinemachine;
+[RequireComponent(typeof(CinemachineVirtualCamera))] // you need to enable cinemachine for this to work
+
+public class CineControl : MonoBehaviour
+{
+	public PlayerData playerdata;
+	private CinemachineVirtualCamera _virtualCamera;
+
+	private void Awake()
+	{
+		virtualCamera = GetComponent<CinemachineVirtualCamera>();
+		playerdata.InstanceAction = InstanceHandler;
+		playerdata.InstancePlayer();
+	}
+	private void InstanceHandler(GameObject obj);
+	{
+		virtualCamera.Follow = object.transform;
+	}
+	
+}
--- proj_test/Assets/Class/Data scripts/PlayerData.cs
+++ proj_test/Assets/Class/Data scripts/PlayerData.cs
@@ -1,12 +1,23 @@
 ﻿
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class PlayerData : GameArtData
 {
+    public UnityAction<GameObject> InstanceAction; // 1
     public FloatData health;
     public ClothData shirt;
     public ClothData pants;
     public List<WeaponData> weapons;
+
+    public void
+    {
+        var newPlayer = Instantiate(prefab);
+        var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        playerSprite.sprite = sprite;
+        playerSprite.color = spriteColor;
+        InstanceAction(newPlayer); // 1
+    }
 }
