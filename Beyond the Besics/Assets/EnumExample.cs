e023e01246bc096587bd1a6dd5daf0a48230eb30
Parker <parkerbennion@gmail.com>
2019-11-07 11:27:19 -0700

    plural site 2D tutorial.
    classes,enum,switch,var,endregion.

---
+++ Beyond the Besics/Assets/EnumExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumExample : MonoBehaviour
+{
+
+	private int gameState;
+	void Start ()
+	{
+		gameState = 0;
+	}
+	
+	void Update () {
+		switch (gameState)
+		{
+			case 2:
+				Debug.Log("The game has ended");
+				break;
+			case 0:
+				Debug.Log("The game is running");
+				break;
+			case 1:
+				Debug.Log("The game is paused");
+				break;
+		}
+	}
+}
+
+public enum GameState { Running , Paused , Ended }
---
+++ Beyond the Besics/Assets/SampleScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeyondTheBasics.Gameplay
+{
+	public class SampleScript : MonoBehaviour
+	{
+
+		#region Variable Declarations
+
+		public int numberToDisplay;
+		private int numberToMultiplyBy;
+
+		#endregion
+
+		#region Private Methods
+
+		private void Start()
+		{
+			SayHelloWorld();
+		}
+
+		private void SayHelloWorld()
+		{
+			var randomNumber = Random.Range(0, numberToDisplay);
+			var displayText = "hello world";
+			for (var i = 0; i < randomNumber; i++)
+			{
+				Debug.Log(displayText);
+			}
+		}
+
+		private void MultiplyNumber(int numberToMultiply)
+		{
+			int product = numberToMultiply * numberToMultiplyBy;
+
+			Debug.Log(product);
+		}
+
+		private void AnonymousTypeExample()
+		{
+			var enemy = new { name = "Monster", hitPoints = 100 };
+		}
+
+		#endregion
+	}
+}
