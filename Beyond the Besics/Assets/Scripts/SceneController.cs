091f538ae3cc050e0494f7e86b5c19275574dc7c
Parker <parkerbennion@gmail.com>
2019-11-10 23:33:04 -0700

    went over arrays and uploaded sprite assests int tutorial.

---
+++ Beyond the Besics/Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+	public string[] shapes = {"circle", "square", "triangle", "octagon"};
+void Start ()
+	{
+		for (int i = 0; i < shapes.Length; i++)
+		{
+			shapes[i] = shapes[i].ToUpper();
+			Debug.Log(shapes[i]);
+		}
+
+	}
+
+	void Update () {
+		
+	}
+}
