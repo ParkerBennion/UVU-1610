04365079a7c3c5d2c26ae8a3e05b0dedec580cef
Parker <parkerbennion@gmail.com>
2019-11-12 16:11:09 -0700

    time delay

---
+++ proj_test/Assets/Class/Coroutines.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.WSA.Input;
+
+public class Coroutines : MonoBehaviour
+{
+	public UnityEvent startEvent, repeatEvent, endEvent;
+	public float seconds = 10f;
+	public int counter = 10;
+	public bool canRun;
+	private WaitForSeconds wfsObj; //look up "references" to understand this. its related to variables
+
+	public void Awake()
+	{
+		StartCoroutine(RunCoroutine());
+	}
+
+	IEnumerator RunCoroutine()
+	{
+		startEvent.Invoke();
+		
+		while (counter > 0)
+		{
+			repeatEvent.Invoke();
+			yield return wfsObj;
+			counter--;
+		}
+
+		while (canRun)
+		{
+			repeatEvent.Invoke();
+			yield return wfsObj;
+			counter--;
+		}
+
+		endEvent.Invoke();
+	}
+}
